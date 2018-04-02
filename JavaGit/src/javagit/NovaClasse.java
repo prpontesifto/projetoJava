b9d6e5f6f07d1fb09172de82f87754c42be709af
Paulo Ricardo <prsilvapontes@gmail.com>
2018-04-01 22:12:24 -0300

    Modificação da classe NovaClasse

--- JavaGit/src/javagit/NovaClasse.java
+++ JavaGit/src/javagit/NovaClasse.java
@@ -11,6 +11,22 @@ package javagit;
  */
 public class NovaClasse {
     
-    String nome;
-    int idade;    
+    private String nome;
+    private int idade;    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
